06a5517112929f74d7771060046d07d8db09dfc4
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-01 21:01:25 -0600

    Add animal

--- hash-examples.rb
+++ hash-examples.rb
@@ -4,4 +4,18 @@ car = {
   "cost" => '$30K'
   "horsepower" => '241'
   "torque" => '273 pound-feet'
+  "colors_available" => ['pearl', 'gray', 'red', 'blue', 'silver', 'black']
+}
+
+animal = {
+  "type" => 'dog'
+  "name" => 'Mia'
+  "age" => '4'
+  "breed" => 'American bulldog'
+  "color" => 'brindle'
+  "energy_level" => 'high'
+  "athleticism" => 'medium'
+  "is_extra" => 'true'
+  "is_calm" => 'false'
+  "personality" => ['weird', 'sweet']
 }
